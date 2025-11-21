f9b22ddc300382fecf08beba5f28222ec343132a
Nour Eldeen Mahomud <noureldeendev@gmail.com>
2025-11-22 01:03:05 +0200

    finished Repositories and unit of work

--- Project/Bookify.Data/Data/AppDbContext.cs
+++ Project/Bookify.Data/Data/AppDbContext.cs
@@ -13,30 +13,19 @@ namespace Bookify.Data.Data
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
-        private readonly IConfiguration _config;
-        public AppDbContext(DbContextOptions<AppDbContext> options,IConfiguration Config) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            _config= Config;
         }
 
         public DbSet<RoomType> RoomTypes { get; set; }
         public DbSet<Room> Rooms { get; set; }
         public DbSet<Booking> Bookings { get; set; }
         public DbSet<BookingPayment> BookingPayments { get; set; }
-        public DbSet<BookingStatusHistory> BookingStatusHistories { get; set; }
+        public DbSet<BookingStatusHistory> BookingStatusHistory { get; set; }
         public DbSet<AuditLog> AuditLogs { get; set; }
         public DbSet<GalleryImage> GalleryImages { get; set; }
 
-
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-            }
-        }
+       
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
